8f9336e8b1401c9107ebf1abb772712be17c5c12
Mexus209 <spikemorow@gmail.com>
2022-04-04 21:18:53 +0300

    tests changes

--- app/src/test/kotlin/programming/task2/AppTest.kt
+++ app/src/test/kotlin/programming/task2/AppTest.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 
 
 class AppTest {
-    private val directoryTest = Paths.get("src\\test\\resources").toAbsolutePath().toString()
+    private val directoryTest = Paths.get("src/test/resources").toAbsolutePath().toString()
     private fun assertFileContent(name: String, expectedContent: String) {
         val file = File(name)
         val content = file.readLines().joinToString("\n")
@@ -17,21 +17,21 @@ class AppTest {
     fun getPermission() {
         assertEquals(
             "r-x",
-            LongFormat("${directoryTest}\\onlyRead.txt").getPermission(true)
+            LongFormat("${directoryTest}/onlyRead.txt").getPermission(true)
         )
         assertEquals(
             "rwx",
             LongFormat(
-                "${directoryTest}\\kotlin.jpg").getPermission(true)
+                "${directoryTest}/kotlin.jpg").getPermission(true)
         )
         assertEquals(
             "500",
-            LongFormat("${directoryTest}\\onlyRead.txt").getPermission(false)
+            LongFormat("${directoryTest}/onlyRead.txt").getPermission(false)
         )
         assertEquals(
             "700",
             LongFormat(
-                "${directoryTest}\\kotlin.jpg").getPermission(false)
+                "${directoryTest}/kotlin.jpg").getPermission(false)
         )
     }
 
@@ -46,30 +46,30 @@ class AppTest {
     fun lastModifiedTime() {
         assertEquals(
             "Sun Apr 03 17:31:26 MSK 2022",
-            LongFormat("${directoryTest}\\emptyFolder").lastModifiedTime()
+            LongFormat("${directoryTest}/emptyFolder").lastModifiedTime()
         )
         assertEquals(
             "Sun Sep 12 22:36:52 MSK 2021",
-            LongFormat("${directoryTest}\\substrings_in1.txt").lastModifiedTime()
+            LongFormat("${directoryTest}/substrings_in1.txt").lastModifiedTime()
         )
     }
 
     @Test
     fun size() {
-        assertEquals("6,4 kB", LongFormat("${directoryTest}\\kotlin.jpg").size(true))
-        assertEquals("1336 B", LongFormat("${directoryTest}\\substrings_in1.txt").size(false))
+        assertEquals("6,4 kB", LongFormat("${directoryTest}/kotlin.jpg").size(true))
+        assertEquals("1336 B", LongFormat("${directoryTest}/substrings_in1.txt").size(false))
     }
 
     @Test
     fun output() {
-        assertEquals(listOf("File/folder is not found"), Output(File("${directoryTest}\\test")).create())
+        assertEquals(listOf("File/folder is not found"), Output(File("${directoryTest}/test")).create())
         assertEquals(listOf(
             "1.txt",
             "2.txt",
             "3.txt"
-        ), Output(File("${directoryTest}\\testFolder")).create())
-        Output(File("${directoryTest}\\testFolder")).fileOutput("${directoryTest}\\output.txt")
-        assertFileContent("${directoryTest}\\output.txt",
+        ), Output(File("${directoryTest}/testFolder")).create())
+        Output(File("${directoryTest}/testFolder")).fileOutput("${directoryTest}/output.txt")
+        assertFileContent("${directoryTest}/output.txt",
         """1.txt
             |2.txt
             |3.txt
