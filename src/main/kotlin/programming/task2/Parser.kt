169335f58005d856a62ca42341bea59d4491dfef
VIN0GR4D <egurt0416@mail.ru>
2022-04-06 22:49:39 +0300

    ready

---
+++ src/main/kotlin/programming/task2/Parser.kt
@@ -0,0 +1,29 @@
+package programming.task2
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import java.io.File
+import java.io.IOException
+
+class Parser: CliktCommand() {
+    private val pack: Boolean by option("-z", help = "file packaging").flag()
+    private val unpack: Boolean by option("-u", help = "file unpacking").flag()
+    private val output: File by argument("-out", help = "output name").file()
+    private val path: File by argument("file", help = "Path to the folder").file(mustExist = true)
+    override fun run() {
+        try {
+            val packRLE = PackRLE()
+            if (pack) {
+            packRLE.assembleFile(pack, path.toString(), output.toString())
+            }
+            if (unpack) {
+                packRLE.assembleFile(unpack, path.toString(), output.toString())
+            }
+        } catch (e: IOException) {
+            System.err.println(e.message)
+        }
+    }
+}
