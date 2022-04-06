c911ab7290f17e30510c19d88c5cff1d33318776
Ars <asd>
2022-04-06 19:36:31 +0300

    v1

---
+++ app/src/main/java/Find/Finder.java
@@ -0,0 +1,60 @@
+package Find;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Finder {
+    private final String pathToDir;
+
+    private final String fileName;
+
+    private final boolean recursive;
+
+    public Finder(String pathToDir, String fileName, boolean recursive) {
+        this.pathToDir = pathToDir;
+        this.fileName = fileName;
+        this.recursive = recursive;
+    }
+
+    public String[] initSearch(){
+        String[] result = null;
+        if (fileName.contains(".")) {result = find(true);} else {result = find(false);}
+        return result;
+    }
+
+    private String[] find( boolean ExtNeeded ) {
+        /* Sorting files and directories */
+        ArrayList<String> results = new ArrayList<>();
+        ArrayList<String> namesOfFiles = new ArrayList<>();
+        ArrayList<String> namesOfDirs = new ArrayList<>();
+        if (new File(pathToDir).listFiles() == null) {
+            throw new IllegalArgumentException("No such directory or directory is empty.");
+        }
+        for (File file : new File(pathToDir).listFiles()) {
+            if (file.isFile()) { namesOfFiles.add(file.getName());}
+            else { namesOfDirs.add(file.getName());}
+        }
+        /* Finding essential files */
+        String name = null;
+        for (String nameOfFile : namesOfFiles) {
+            if (ExtNeeded) {
+                name = nameOfFile;
+            } else {
+                name = nameOfFile.split("\\.")[0];
+            }
+            if (name.equals(fileName)) {
+                results.add(pathToDir + "\\" + nameOfFile);
+            }
+        }
+        /* Initializing recursive search */
+        if (!namesOfDirs.isEmpty() && recursive) {
+            for (String dir : namesOfDirs){
+                Finder f = new Finder(pathToDir+"\\"+dir, fileName, recursive);
+                results.addAll(List.of(f.find(ExtNeeded)));
+            }
+        }
+        return results.toArray(new String[0]);
+    }
+
+}
---
+++ app/src/main/java/Find/FinderLauncher.java
@@ -0,0 +1,50 @@
+package Find;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.CmdLineParser;
+
+import java.util.ArrayList;
+
+public class FinderLauncher {
+
+    @Option(name="-r",usage="Also search in subdirectories")
+    private boolean recursive;
+
+    @Option(name="-d", usage="Directory to search in",metaVar="Directory")
+    private String pathToDir = "";
+
+    @Argument(required = true, usage = "Name of file", metaVar = "fileName")
+    private String fileName;
+
+    public static void main(String[] args) {
+        try {
+            new FinderLauncher().parseAndLaunch(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void parseAndLaunch(String[] args) {
+        CmdLineParser cmdParser = new CmdLineParser(this);
+
+        try {
+            cmdParser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.out.println(e.getMessage());
+            cmdParser.printUsage(System.err);
+            return;
+        }
+
+        Finder finder = new Finder(pathToDir, fileName, recursive);
+        String[] results = finder.initSearch();
+
+        for (String str : results){
+            System.out.println(str);
+        }
+    }
+
+}
+
+
