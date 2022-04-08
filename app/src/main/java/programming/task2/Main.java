0617c0e1ba8cbca2833ec49aa8e7144ba38c95a4
AnastasiiaBalagaeva <abalagaevaa@mail.ru>
2022-04-08 20:02:33 +0300

    123

--- app/src/main/java/programming/task2/Main.java
+++ app/src/main/java/programming/task2/Main.java
@@ -2,11 +2,15 @@ package programming.task2;
 
 import picocli.CommandLine;
 
-import java.io.File;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src//test//resources//text3.txt");
-        new CommandLine(new HelloCommand()).execute("--si", file.toString());
+//        String result = "";
+//        for (String arg : args) {
+//            result += arg;
+//        }
+//        String lastWord = result.substring(result.lastIndexOf(" ")+1);
+        new CommandLine(new HelloCommand()).execute(args);
     }
 }
