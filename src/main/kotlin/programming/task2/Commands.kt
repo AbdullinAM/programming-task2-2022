8c1110064f28d2712cd384df1c9e2e58db7650c5
MikleAKA <mixa1410buz@mail.ru>
2022-04-10 23:48:57 +0300

    Test gradle

--- src/main/kotlin/programming/task2/Commands.kt
+++ src/main/kotlin/programming/task2/Commands.kt
@@ -5,11 +5,13 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.path
 import java.io.File
+import java.nio.file.Path
 
 class Commands: CliktCommand() {
     private val word: String by argument("Word or phrase", "Word or phrase to search")
-    private val path: File by argument("File", "Path to folder").file(mustExist = true)
+    private val path: Path by argument("File", "Path to folder").path(mustExist = true)
     private val regex: Boolean by option("-r", help = "Write phrase to search").flag()
     private val invert: Boolean by option("-v", help = "Search string where is not your word").flag()
     private val ignore: Boolean by option("-i", help = "Ignore case").flag()
